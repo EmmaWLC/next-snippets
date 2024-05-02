8ab43af7383d69c7b9a9f35167f2ab20bc8962c3
Emma Chen <emmachen@starlux-airlines.com>
2024-05-02 11:59:07 +0800

    revalidatePath

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,5 +1,6 @@
 'use server';
 
+import { revalidatePath } from 'next/cache';
 import { db } from '@/db';
 import { redirect } from 'next/navigation';
 
@@ -17,6 +18,7 @@ export async function deleteSnippet(id: number) {
     where: { id },
   });
 
+  revalidatePath('/');
   redirect('/');
 }
 
@@ -59,7 +61,7 @@ export async function createSnippet(
       };
     }
   }
-
+  revalidatePath('/');
   // Redirect the user back to the root route
   redirect('/');
 }
